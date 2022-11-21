d4d25f01caf8afea923e258485ef4b9cc890f42f
Anish Kulkarni <anish.j.kulkarni@gmail.com>
2022-11-20 18:23:26 -0800

    added tests to lab8 and unit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Lab 8 - Starter
+
+Anish Kulkarni
+
+Teammates : N/A
+
+1. I would run the tests within a Github action that runs whenever code is pushed. This is the most effective approach, as it is more efficient than running all tests locally, but it is effective as well, because we test all the new code pushed before it gets added to the progject - ensuring code is properly checked before merging.
+
+
+2. No, an end-to-end test is too complex for a simply return value test. It is more effective to use a unit test. 
+
+
+3. No, a unit test is too small scale to test a full messaging feature. We can potentially test small return values of helper functions, but the whole feature needs an E2E test. 
+
+
+4. Yes, as it is just one small value, which we can test with Jest easily - no helper functions to consider. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,123 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// Tests real phone number #1
+test('realPhoneNumber1', () => {
+    phoneNumber = '408-229-9934'
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(true);
+});
+
+// Tests real phone number #2
+test('realPhoneNumber2', () => {
+    phoneNumber = '111-111-1111'
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(true);
+});
+
+// Tests false phone number #1
+test('fakePhoneNumber1', () => {
+    phoneNumber = '123'
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(false);
+});
+
+// Tests false phone number #2
+test('fakePhoneNumber2', () => {
+    phoneNumber = '123-456-789'
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(false);
+});
+
+// Tests real email #1
+test('realEmail1', () => {
+    emailString = 'ajk@gmail.com'
+    expect(functions.isEmail(emailString)).toBe(true);
+});
+
+// Tests real email #2
+test('realEmail2', () => {
+    emailString = 'test_Name123@example.com'
+    expect(functions.isEmail(emailString)).toBe(true);
+});
+
+// Tests fake email #1
+test('fakeEmail1', () => {
+    emailString = 'name@yahoo.com@gmail.com';
+    expect(functions.isEmail(emailString)).toBe(false);
+});
+
+// Tests fake email #2
+test('fakeEmail2', () => {
+    emailString = 'thisisnotanemail.123'
+    expect(functions.isEmail(emailString)).toBe(false);
+});
+
+// Tests strong password #1
+test('strongPass1', () => {
+    passString = 'my_password'
+    expect(functions.isStrongPassword(passString)).toBe(true);
+});
+
+// Tests strong password #2
+test('strongPass2', () => {
+    passString = 'letmeinplease'
+    expect(functions.isStrongPassword(passString)).toBe(true);
+});
+
+// Tests weak password #1
+test('weakPass1', () => {
+    passString = 'thispasswordisjustwaytoolongtouse';
+    expect(functions.isStrongPassword(passString)).toBe(false);
+});
+
+// Tests weak password #2
+test('weakPass2', () => {
+    passString = '@mypass'
+    expect(functions.isStrongPassword(passString)).toBe(false);
+});
+
+// Tests real date #1
+test('realDate1', () => {
+    date = '20/11/2022'
+    expect(functions.isDate(date)).toBe(true);
+});
+
+// Tests real date #2
+test('realDate2', () => {
+    date = '09/24/2001'
+    expect(functions.isDate(date)).toBe(true);
+});
+
+// Tests fake date #1
+test('fakeDate1', () => {
+    date = '01/01/01';
+    expect(functions.isDate(date)).toBe(false);
+});
+
+// Tests fake date #2
+test('fakeDate2', () => {
+    date = 'thisisntadate'
+    expect(functions.isDate(date)).toBe(false);
+});
+
+// Tests real hex color string #1
+test('realHex1', () => {
+    hexString = 'FFA82B'
+    expect(functions.isHexColor(hexString)).toBe(true);
+});
+
+// Tests real hex color string #2
+test('realHex2', () => {
+    hexString = 'a2c'
+    expect(functions.isHexColor(hexString)).toBe(true);
+});
+
+// Tests fake hex color string #1
+test('fakeHex1', () => {
+    hexString = 'RGB';
+    expect(functions.isHexColor(hexString)).toBe(false);
+});
+
+// Tests fake hex color string #2
+test('fakeHex2', () => {
+    hexString = 'F'
+    expect(functions.isHexColor(hexString)).toBe(false);
+});
